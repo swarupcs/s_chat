4579b6bb7f10e276d96ea8999dc429c8baf44b46
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-09-16 21:44:30 +0530

    Displaying Mobile Contacts List(2440)

--- lib/widgets/contacts_list.dart
+++ lib/widgets/contacts_list.dart
@@ -16,26 +16,29 @@ class ContactList extends StatelessWidget {
               shrinkWrap: true,
                 itemCount: info.length,
                 itemBuilder: (context, index){
-                    return Padding(
-                      padding: const EdgeInsets.only(bottom: 8.0),
-                      child: ListTile(
-                        title: Text(
-                            info[index]['name'].toString(),
-                            style: const TextStyle(fontSize: 18)
-                        ),
-                        subtitle:Padding(
-                          padding: EdgeInsets.only(top: 6),
-                          child: Text(info[index]['message'].toString(),style: const TextStyle(fontSize: 15)),
-                        ),
-                        leading: CircleAvatar(
-                          backgroundImage: NetworkImage(
-                            info[index]['profilePic'].toString()
+                    return InkWell(
+                      onTap: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.only(bottom: 8.0),
+                        child: ListTile(
+                          title: Text(
+                              info[index]['name'].toString(),
+                              style: const TextStyle(fontSize: 18)
                           ),
-                        ),
-                        trailing: Text(
-                            info[index]['time'].toString(),
-                            style: const TextStyle(fontSize: 13, color: Colors.grey),
+                          subtitle:Padding(
+                            padding: EdgeInsets.only(top: 6),
+                            child: Text(info[index]['message'].toString(),style: const TextStyle(fontSize: 15)),
+                          ),
+                          leading: CircleAvatar(
+                            backgroundImage: NetworkImage(
+                              info[index]['profilePic'].toString()
+                            ),
+                          ),
+                          trailing: Text(
+                              info[index]['time'].toString(),
+                              style: const TextStyle(fontSize: 13, color: Colors.grey),
 
+                          ),
                         ),
                       ),
                     );
